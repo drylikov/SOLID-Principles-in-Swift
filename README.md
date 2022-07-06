9c4947422b27e88078470220a9c9e91b4c00abee
drylikov <denis.rylikov@protonmail.com>
2022-07-07 01:19:06 +0300

    SOLID Principles in Swift

---
+++ README.md
@@ -0,0 +1,23 @@
+# The SOLID Principles in Swift
+
+## Description
+
+Reviewing the SOLID principles with examples in Swift.
+
+Accompanies this video: [SOLID Principles in Swift](https://www.youtube.com/watch?v=gkxmeWvGEpU&t=2s)
+
+## Credits
+
+* Much inspiration was taken from [Practically SOLID](https://speakerdeck.com/dannyhertz/practically-solid-1) by Danny Hertz
+
+## License
+
+This project is Copyright (c) 2016 Prolific Interactive. It may be redistributed under the terms specified in the [LICENSE] file.
+
+[LICENSE]: /LICENSE
+
+## Maintainers
+
+![prolific](https://s3.amazonaws.com/prolificsitestaging/logos/Prolific_Logo_Full_Color.png)
+
+This project is maintained and funded by Prolific Interactive. The names and logos are trademarks of Prolific Interactive.
---
+++ SOLIDExample/SOLIDExample/TwitterExample/SOLID/SOLIDTimelineService.swift
@@ -0,0 +1,39 @@
+//
+//  SOLIDTimelineService.swift
+//  SOLIDExample
+//
+//  Created by Harlan Kellaway on 1/12/16.
+//  Copyright © 2016 Prolific Interactive. All rights reserved.
+//
+
+import Foundation
+
+struct SOLIDTimelineService: Service {
+    
+    private let freq: Int
+    let userID: String
+    
+    init(frequency: Int, userID: String) {
+        self.freq = frequency
+        self.userID = userID
+    }
+    
+    // MARK: - Protocol conformance
+    
+    // MARK: Service
+    
+    func frequency() -> Int {
+        return freq
+    }
+    
+    func execute() {
+        fetchTimelineForUserID(userID)
+    }
+    
+    // MARK: - Private methods
+    
+    private func fetchTimelineForUserID(userID: String) {
+        print("[SOLID] fetched timeline for user \(userID)")
+    }
+    
+}
